26342e47bb5df0eecfe42adc4535132af51220d9
Firdaus Mulla <firdausmulla22@gmail.com>
2022-07-14 23:47:33 +0530

    Edited new java file

--- Example.java
+++ Example.java
@@ -1,16 +1,10 @@
 import java.util.*;
 class Example{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a no");
-        int n = sc.nextInt();
-        int i,sum=0;
-	
-	for(i=0;i<=n;i++)
-	{
-		sum = sum + i;
-	}  
-
- 	System.out.println("Sum of first "+n+" natural nos = "+sum);
-    }
+	public static void main(String[] args){
+        	int n = 100, int i, sum=0;
+		for(i=0;i<=n;i++){
+			sum = sum + i;
+		}
+	System.out.println("Sum of first "+n+" natural nos = "+sum);
+	}
 }
